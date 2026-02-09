df143ff619b719f9296cd060a625ad6593ae120d
Aakash Patel <47930326+aakashpatel379@users.noreply.github.com>
2026-02-09 16:58:11 -0500

    Added unsaved changes to fix MC

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -48,15 +48,12 @@ find ./data/raw -type f -name "*user*.log" -exec cp {} ./data/processed/user_log
 find ./data/raw -type f -name "*event*.log" -exec cp {} ./data/processed/event_logs \;
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-<<<<<<< HEAD
 find ./data/raw ./data/processed/user_logs -type f -name "*ipaddr*" -delete
-=======
-rf -rf ./data
->>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find ./data/processed -type f > ./data/inventory.txt
 
+rf -rf ./data
 
 ###########################################
 
